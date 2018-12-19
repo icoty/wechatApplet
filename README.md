a557bc0a8237ede8d6edd2fae4b9b907ed61f49a
似水似月似明镜 <604381117@qq.com>
2018-12-19 21:10:34 +0800

    签到系统 微信小程序首次提交

---
+++ README.md
@@ -0,0 +1,3 @@
+1. client 微信开发者工具调试用的客户端
+2. djangoServer 基于python的django服务端
+3. mvcServer 基于js的mvx服务端, 当前所用, 启动命令: npm run dev; 后期需要转移成django
---
+++ mvcServer/config.js
@@ -0,0 +1,54 @@
+const CONF = {
+    port: '5757',
+    rootPathname: '',
+
+    // 微信小程序 App ID
+    appId: 'wx552a0d4b18e3f9e7',
+
+    // 微信小程序 App Secret
+    appSecret: '1a2667e5a3c5528bfe34251fd561accb',
+
+    // 是否使用腾讯云代理登录小程序
+    useQcloudLogin: true,
+
+    /**
+     * MySQL 配置，用来存储 session 和用户信息
+     * 若使用了腾讯云微信小程序解决方案
+     * 开发环境下，MySQL 的初始密码为您的微信小程序 appid
+     */
+    mysql: {
+        host: '172.21.0.9',
+        port: 3306,
+        user: 'root',
+        db: 'cAuth',
+        pass: 'wx552a0d4b18e3f9e7',
+        char: 'utf8mb4'
+    },
+
+    cos: {
+        /**
+         * 区域
+         * @查看 https://cloud.tencent.com/document/product/436/6224
+         */
+        region: 'ap-beijing1',
+        // Bucket 名称
+        fileBucket: 'qcloudtest',
+        // 文件夹
+        uploadFolder: ''
+    },
+
+    // 微信登录态有效期
+    wxLoginExpires: 7200,
+    // 其他配置 ...
+    serverHost: 'www.2belief.com',
+    tunnelServerUrl: 'https://tunnel.ws.qcloud.la',
+    tunnelSignatureKey: '27fb7d1c161b7ca52d73cce0f1d833f9f5b5ec89',
+  // 腾讯云相关配置可以查看云 API 秘钥控制台：https://console.qcloud.com/capi
+    qcloudAppId: '1257300696',
+    qcloudSecretId: 'AKID6LLO3y6kraCwfngcw7RWkcaI8PYxIKLC',
+    qcloudSecretKey: 'xiGfdtBIUdSDMHY35kDfPzTZuhgThG8w',
+    wxMessageToken: 'weixinmsgtoken',
+    networkTimeout: 30000
+}
+
+module.exports = CONF
