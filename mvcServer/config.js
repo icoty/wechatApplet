9e512db3030da132978251a4fba55395ac4d948b
icoty <icoty.yangy@gmail.com>
2019-03-24 16:48:50 +0800

    mysql ip config

--- mvcServer/config.js
+++ mvcServer/config.js
@@ -17,7 +17,7 @@ const CONF = {
      * 开发环境下，MySQL 的初始密码为您的微信小程序 appid
      */
     mysql: {
-        host: '127.0.0.1',
+        host: '45.40.199.171',
         port: 3306,
         user: 'root',
         db: 'cAuth',
